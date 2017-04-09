4ee265619b88858ca0f300e471fbc4b5f9fa9c3b
Krzysztof Kossowski <kkossows@gmail.com>
2017-04-09 13:49:53 +0200

    Server_CoAP vol2

--- uno/Server_CoAP/Server_CoAP.ino
+++ uno/Server_CoAP/Server_CoAP.ino
@@ -5,13 +5,17 @@
 
   Including: 
   - wireless connection via nRF24L01 module with OBJECT;
-  - wired connection via ETHERNET with INTERNET via router; 
+  - wired connection via ETHERNET W5100 with INTERNET via router; 
 */
 
 // include traditionall librares;
+#include <SPI.h>
+
 #include <RF24Network.h> 
 #include <RF24.h> 
-#include <SPI.h>
+
+#include <Ethernet.h>
+#include <EthernetUdp.h>
 
 // include our librares; 
 #include CoapParser_h
@@ -30,53 +34,75 @@ uint8_t lampState;  //0 - ON, 1 - OFF
 uint32_t buttonState; // incomming time marker  
 
 
-void setup() {
-  
-  initializeWirelessCommunication();
+// variable connected with wired connection
+EthernetUDP Udp;
+byte mac[] = {00,0xaa,0xbb,0xcc,0xde,0xf3};
+short localPort=1237;
+const uint8_t MAX_BUFFER = 100; //do zastanowienia
+char ethMessage[MAX_BUFFER];
 
+void setup() {  
+  initializeRF24Communication();
+  initializeEthernetCommunication();
 }
 
 void loop() {
-  
   // checking the network object regularly;
   network.update();
-  // if there is a message for us, process it;
-  processWirelessMessage();
-
-
-
+  
 }
 
 
 
 // RF24 Methodes--------------------------------
 // initiation of wireless variables and network connections
-void initializeWirelessCommunication(){
+void initializeRF24Communication(){
   SPI.begin();
   radio.begin();
   network.begin(RF_CHANNEL, THIS_NODE_ID); 
 }
 
-// processing RF24_message
-void processWirelessMessage(){ 
+void receiveRF24Message() {
   while ( network.available() )
   {
-    // get rf24Message
     RF24NetworkHeader header;
     byte rf24Message;
     network.read(header, rf24Message, sizeof(rf24Message));
 
-    // process rf24Message
+    processRF24Message(byte rf24Message);
+  }
+}
+void processRF24Message(byte message){ 
     if ( ((rf24Message & 0x02) >> 1) == LAMP ){
       lampState = (rf24Message & 0x01);
     }
     else {
       buttonState = millis();
     }
-  } 
+}
+void sendRF24Message(byte message) {
+    RF24NetworkHeader header;
+    network.write(header, &message, sizeof(message));
 }
 // END RF23_Methodes----------------------------
 
 // Ethernet_Methodes----------------------------
+void initializeEthernetCommunication(){
+  Ethernet.begin(mac);
+  Udp.begin(localPort);
+}
+void receiveEthernetMessage() {
+  int packetSize = Udp.parsePacket(); //the size of a received UDP packet, 0 oznacza nieodebranie pakietu
+  if(packetSize) {
+    if(packetSize >= 4) {
+      Udp.read(ethMessage, MAX_BUFFER) //do zastanowienia 
+      processEthernetMessage();  
+    }
+}
+void processEthernetMessage(){ 
+}
+void sendEthernetMessage(byte message){
+  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+}
 // END Ethernet_Methodes------------------------
 
